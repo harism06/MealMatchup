23d73c04a83b62c97f335a8fb2cd997c516d6be8
Haris M. <malik.h075659@stu.sanjac.edu>
2023-10-21 18:44:40 -0500

    added json file for data

--- restaurant/restaurant-info.js
+++
@@ -1,8 +0,0 @@
-import allData from "../home/script.js"
-
-function populateRestuarantData() {
-    let titleRestuarant = document.createElement("h1")
-    titleRestuarant.innerHTML = allData.name
-}
-
-window.onload = populateRestuarantData
